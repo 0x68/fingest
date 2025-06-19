efef22052c567b881ffc3766d8143a5e8ae357a8
Tim Fiedler <tim@0x68.de>
2025-06-19 12:44:36 +0200

    Fx format

--- src/fingest/types.py
+++ src/fingest/types.py
@@ -1,12 +1,9 @@
-
-
 class BaseFixture(object):
     def __init__(self, data):
         self.data = data
 
 
 class JSONFixture(BaseFixture):
-
     def keys(self):
         return self.data.keys()
 
