e47379b05158fda5a6fa0de39911ff4a31171ad6
Tim Fiedler <tim@fckp>
2025-06-19 12:28:55 +0200

    First Commit

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+from fingest.plugin import data_fixture
+from fingest.types import BaseFixture, JSONFixture
+
+
+@data_fixture("test.json", description="Das ist ein Test der fehl schlaegt")
+class JsonData(JSONFixture):
+    ...
+
+
+@data_fixture("test.xml", description="Das ist ein Test der fehl schlaegt")
+class XMLData(BaseFixture):
+    ...
+
+
+@data_fixture("test.csv", description="CSV File which is Dirty as Fck")
+class CSV(BaseFixture):
+    """CSV File"""
+    ...
+
+
+@data_fixture("test.json", description="Func Bases")
+def json_test_file(data):
+    return data
